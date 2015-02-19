b049f5252092a4cf03f98f20738684f4469c330a
Gregory Moon <gregory.r.moon@gmail.com>
2015-02-19 00:36:54 -0700

    can get all files from sample dir

--- TweeterApp.cpp
+++ TweeterApp.cpp
@@ -1,14 +1,47 @@
 #include <cstdio>
 #include <string>
+#include <dirent.h>
 
 #include "User.h"
 #include "Streamer.h"
 
-int main(){
-	Streamer::start();
+void readAllUsersFromFile(std::string dirPath);
+void readUserFromPath(std::string filepath);
 
-	User *u1 = new User("handle");
+int main(int argc, char* argv[]){
+	//get dir location from user input
+	std::string dirPath;
 
-	Streamer::join();
+	if(argc == 2)
+		dirPath = argv[1];
+	else
+		 dirPath = "/home/grmoon/school/cse430/projects/2/sample_input/";
+	
+	readAllUsersFromFile(dirPath);
+
+	//Streamer::start();
+
+	//User *u1 = new User("handle");
+
+	//Streamer::join();
+}
+
+void readAllUsersFromFile(std::string dirPath){
+	DIR *dir = opendir(dirPath.c_str());
+
+	if(!dir){
+		perror("opendir");
+		exit(1);
+	}
+	else{
+		struct dirent *dirEntry;
+		while((dirEntry = readdir(dir)) != NULL){
+			printf("%s\n", dirEntry->d_name);
+		}
+	}		
+}
+
+void readUserFromPath(std::string filepath){
+
 }
 
